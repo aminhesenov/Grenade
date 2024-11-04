43648365efbc74e7c0584b6ce804044b82b54f44
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-11-04 11:39:59 +0400

    Grenade
    Grenade game

---
+++ Grenade/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Grenade
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            character = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)character).BeginInit();
+            SuspendLayout();
+            // 
+            // character
+            // 
+            character.Image = Resources.runner_right;
+            character.Location = new Point(318, 349);
+            character.Name = "character";
+            character.Size = new Size(100, 50);
+            character.TabIndex = 0;
+            character.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 2000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 5;
+            timer2.Tick += timer2_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 411);
+            Controls.Add(character);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Grenade";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)character).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox character;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Grenade/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System.Media;
+
+namespace Grenade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random xRandom = new Random();
+        PictureBox grenade;
+        PictureBox grenade2;
+ 
+        SoundPlayer soundPlayer = new SoundPlayer(
+            "D:\\C++\\Grenade\\Grenade\\Resources\\explosion.wav");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { 
+                character.Location=new Point(character.Location.X-7, character.Location.Y);   
+                character.Image=Resources.runner_left;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                character.Location = new Point(character.Location.X + 7, character.Location.Y);
+                character.Image = Resources.runner_right;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*G1*/
+            grenade = new PictureBox();
+            grenade.Image = Resources.grenade;
+            grenade.SizeMode = PictureBoxSizeMode.CenterImage;
+            grenade.Size = new Size(50, 50);
+            grenade.Location = new Point(xRandom.Next(0, 750), 20);
+
+            /*G2*/
+            grenade2 = new PictureBox();
+            grenade2.Image = Resources.grenade;
+            grenade2.SizeMode = PictureBoxSizeMode.CenterImage;
+            grenade2.Size = new Size(50, 50);
+            grenade2.Location = new Point(xRandom.Next(0, 750), 20);
+
+
+            this.Controls.Add(grenade);
+            this.Controls.Add(grenade2);
+            
+        }
+        int count=0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (grenade != null && grenade2 != null)
+            {
+                
+                grenade.Location = new Point(grenade.Location.X, grenade.Location.Y + 7);
+                grenade2.Location = new Point(grenade2.Location.X, grenade2.Location.Y + 7);
+
+                if (440 < grenade2.Location.Y && 440 < grenade.Location.Y)
+                {
+                    count++;
+                }
+                
+                if (character.Bounds.IntersectsWith(grenade.Bounds) ||
+                    character.Bounds.IntersectsWith(grenade.Bounds))
+                {
+                    
+                    grenade.Dispose();
+                    grenade2.Dispose();
+
+                    timer1.Stop();
+                    timer2.Stop();
+
+                    soundPlayer.Play();
+
+                    MessageBox.Show("Oyun bitti, Uduzdunuz! "+count, "Bilgi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    this.Dispose();
+                }
+            }
+
+        }
+
+
+    }
+}
